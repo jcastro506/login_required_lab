0460d527501b49d83df09465760d654272eff46d
jcastro506 <castrojoshua805.com>
2020-11-27 16:40:00 -0500

    something wrong in helper metehods

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'application#hello'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout' to: 'sessions#destroy'
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
